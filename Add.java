6c55177f4c1380beda7a82689e07a37d1ba9c72a
Rajinikr <130490389+Rajinikr@users.noreply.github.com>
2023-04-11 13:37:49 -0400

    Imported files from eclipse

---
+++ Add.java
@@ -0,0 +1,30 @@
+public class Add
+{
+	public static void main(String[] args)
+	{
+		int number1 = 10;
+		int number2 = 14;
+		int number3 =2;
+		//number1++;
+		//number1++;
+		//number2--;
+		
+		int add = number1 + number2;
+		int sub = number1 - number2;
+		int mul = number1 * number2;
+		int dev = number2 / number1;
+		int mod = number2 % number1;
+		int cal = number1+number2/number3;
+		System.out.println(cal);
+		
+		int cal2 = (number1+number2)/number3;
+		System.out.println(cal2);
+		
+		System.out.println("The addition of " + number1 + " and " + number2 + " is " + add);
+		System.out.printf("\n The addition of %d and %d is %d",number1, number2,add);
+		System.out.printf("\n The subtraction of %d and %d is %d",number1, number2,sub);
+		System.out.printf("\n The multiplication of %d and %d is %d",number1, number2,mul);
+		System.out.printf("\n The devision of %d and %d is %d",number1, number2,dev);
+		System.out.printf("\n The modulus of %d and %d is %d",number1, number2,mod);
+	}
+}
---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+Program name: Helloworld
+Description: printing a messege on the screen
+Author:Rajini Ramesh
+Date: 9th Jan 2023
+*/
+
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		//type your program here
+		System.out.print("Hello world. its me "); printing a messege
+	}// end main
+}// end class
